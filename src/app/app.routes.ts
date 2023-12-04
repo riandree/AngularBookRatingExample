ae9d1f1f654a0b959c46ec7112462656f3953088
Andre <andre.rieck@gmx.de>
2023-12-04 11:57:12 +0100

    search und create vorbereitet

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,18 @@
 import { Routes } from '@angular/router';
-import { booksRoutes } from './books/books.routes';
 
 export const routes: Routes = [
     {
         path : '',
         redirectTo : 'books', 
         pathMatch : 'full'
-    },
-    ...booksRoutes
+    }, {
+        path : 'books',
+        // Lazy Loading des Books Feature
+        // Achtung !! Hier darf oben KEIN Import auf z.b. books.routes.ts stattfinden da dann
+        // wieder der Code des Books-Feature direkt gebundelt werden würde!
+        // Hier darf der Import also erst programmatisch zur Laufzeit stattfinden!!
+        // Bei dem Import hier handelt es sich um einen dynamischen Typescript-Import
+        // (z.b. https://mariusschulz.com/blog/dynamic-import-expressions-in-typescript)
+        loadChildren : () => import('./books/books.routes').then(m => m.booksRoutes)
+    }
 ];
--- src/app/books/books.routes.ts
+++ src/app/books/books.routes.ts
@@ -1,8 +1,13 @@
 import { Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { BookDetailsComponent } from "./book-details/book-details.component";
+import { BookCreateComponent } from "./book-create/book-create.component";
+import { BookSearchComponent } from "./book-search/book-search.component";
 
 export const booksRoutes : Routes = [
-    { path: 'books', component: DashboardComponent },
-    { path: 'books/:isbn', component: BookDetailsComponent },
+    { path: '', component: DashboardComponent },
+    { path: 'create', component: BookCreateComponent },
+    { path: 'search', component: BookSearchComponent },
+    { path: ':isbn', component: BookDetailsComponent }
+
 ];
